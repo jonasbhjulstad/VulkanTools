c21441cfda73df6a610e04f9aad46e8ad516782a
Jonas <jonasbhjulstad@gmail.com>
2022-06-28 01:10:00 +0200

    Initial

---
+++ Structures/VulkanSwapChain.hpp
@@ -0,0 +1,68 @@
+/*
+* Class wrapping access to the swap chain
+* 
+* A swap chain is a collection of framebuffers used for rendering and presentation to the windowing system
+*
+* Copyright (C) 2016-2017 by Sascha Willems - www.saschawillems.de
+*
+* This code is licensed under the MIT license (MIT) (http://opensource.org/licenses/MIT)
+*/
+
+#ifndef Vulkan_SWAPCHAIN_HPP
+#define Vulkan_SWAPCHAIN_HPP
+
+#include <stdlib.h>
+#include <string>
+#include <assert.h>
+#include <stdio.h>
+#include <vector>
+#include <iostream>
+
+#include <vulkan/vulkan.hpp>
+#include <GLFW/glfw3.h>
+#include "VulkanInitializers.hpp"
+#include "VulkanTools.hpp"
+#include <imgui_impl_vulkan.h>
+
+
+
+typedef struct _SwapChainBuffers {
+	VkImage image;
+	VkImageView view;
+} SwapChainBuffer;
+
+class VulkanSwapChain
+{
+private: 
+	VkInstance instance;
+	VkDevice device;
+	VkPhysicalDevice physicalDevice;
+	VkSurfaceKHR surface;
+	// Function pointers
+	PFN_vkGetPhysicalDeviceSurfaceSupportKHR fpGetPhysicalDeviceSurfaceSupportKHR;
+	PFN_vkGetPhysicalDeviceSurfaceCapabilitiesKHR fpGetPhysicalDeviceSurfaceCapabilitiesKHR; 
+	PFN_vkGetPhysicalDeviceSurfaceFormatsKHR fpGetPhysicalDeviceSurfaceFormatsKHR;
+	PFN_vkGetPhysicalDeviceSurfacePresentModesKHR fpGetPhysicalDeviceSurfacePresentModesKHR;
+	PFN_vkCreateSwapchainKHR fpCreateSwapchainKHR;
+	PFN_vkDestroySwapchainKHR fpDestroySwapchainKHR;
+	PFN_vkGetSwapchainImagesKHR fpGetSwapchainImagesKHR;
+	PFN_vkAcquireNextImageKHR fpAcquireNextImageKHR;
+	PFN_vkQueuePresentKHR fpQueuePresentKHR;
+public:
+	VkFormat colorFormat;
+	VkColorSpaceKHR colorSpace;
+	VkSwapchainKHR swapChain = VK_NULL_HANDLE;	
+	uint32_t imageCount = 0;
+	std::vector<VkImage> images;
+	std::vector<SwapChainBuffer> buffers;
+	uint32_t queueNodeIndex = UINT32_MAX;
+
+	void initSurface(VkInstance instance, GLFWwindow* window, VkSurfaceKHR surface, uint32_t width, uint32_t height);
+	void connect(VkInstance instance, VkPhysicalDevice physicalDevice, VkDevice device);
+	void create(ImGui_ImplVulkanH_Window* wd, uint32_t* width, uint32_t* height, bool vsync = false);
+	VkResult acquireNextImage(VkSemaphore presentCompleteSemaphore, uint32_t* imageIndex);
+	VkResult queuePresent(VkQueue queue, uint32_t imageIndex, VkSemaphore waitSemaphore = VK_NULL_HANDLE);
+	void cleanup();
+};
+
+#endif // Vulkan_SWAPCHAIN_HPP
---
+++ gltf/VulkanTexture.hpp
@@ -0,0 +1,91 @@
+/*
+* Vulkan texture loader
+*
+* Copyright(C) by Sascha Willems - www.saschawillems.de
+*
+* This code is licensed under the MIT license(MIT) (http://opensource.org/licenses/MIT)
+*/
+
+#ifndef Vulkan_TEXTURE_HPP
+#define Vulkan_TEXTURE_HPP
+
+#include <fstream>
+#include <stdlib.h>
+#include <string>
+#include <vector>
+
+#include <vulkan/vulkan.hpp>
+
+#include <ktx.h>
+#include <ktxvulkan.h>
+
+#include <VulkanBuffer.hpp>
+#include <VulkanDevice.hpp>
+
+class Texture
+{
+  public:
+	VulkanDevice *   device;
+	VkImage               image;
+	VkImageLayout         imageLayout;
+	VkDeviceMemory        deviceMemory;
+	VkImageView           view;
+	uint32_t              width, height;
+	uint32_t              mipLevels;
+	uint32_t              layerCount;
+	VkDescriptorImageInfo descriptor;
+	VkSampler             sampler;
+
+	void      updateDescriptor();
+	void      destroy();
+	ktxResult loadKTXFile(std::string filename, ktxTexture **target);
+};
+
+class Texture2D : public Texture
+{
+  public:
+	void loadFromFile(
+	    std::string        filename,
+	    VkFormat           format,
+	    VulkanDevice *device,
+	    VkQueue            copyQueue,
+	    VkImageUsageFlags  imageUsageFlags = VK_IMAGE_USAGE_SAMPLED_BIT,
+	    VkImageLayout      imageLayout     = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL,
+	    bool               forceLinear     = false);
+	void fromBuffer(
+	    void *             buffer,
+	    VkDeviceSize       bufferSize,
+	    VkFormat           format,
+	    uint32_t           texWidth,
+	    uint32_t           texHeight,
+	    VulkanDevice *device,
+	    VkQueue            copyQueue,
+	    VkFilter           filter          = VK_FILTER_LINEAR,
+	    VkImageUsageFlags  imageUsageFlags = VK_IMAGE_USAGE_SAMPLED_BIT,
+	    VkImageLayout      imageLayout     = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
+};
+
+class Texture2DArray : public Texture
+{
+  public:
+	void loadFromFile(
+	    std::string        filename,
+	    VkFormat           format,
+	    VulkanDevice *device,
+	    VkQueue            copyQueue,
+	    VkImageUsageFlags  imageUsageFlags = VK_IMAGE_USAGE_SAMPLED_BIT,
+	    VkImageLayout      imageLayout     = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
+};
+
+class TextureCubeMap : public Texture
+{
+  public:
+	void loadFromFile(
+	    std::string        filename,
+	    VkFormat           format,
+	    VulkanDevice *device,
+	    VkQueue            copyQueue,
+	    VkImageUsageFlags  imageUsageFlags = VK_IMAGE_USAGE_SAMPLED_BIT,
+	    VkImageLayout      imageLayout     = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
+};
+#endif
