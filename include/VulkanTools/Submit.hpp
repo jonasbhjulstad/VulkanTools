cb2f6d44735e75567c13b4ac324ae4009d85c1de
jonas <jonasbhjulstad@gmail.com>
2024-07-22 01:15:27 +0200

    v1.0.21

--- include/VulkanTools/Submit.hpp
+++ include/VulkanTools/Submit.hpp
@@ -2,7 +2,6 @@
 #include <VulkanTools/Instance.hpp>
 namespace VkVP {
 // submits registered command buffers to the queue
-void submitBuffers(VulkanInstance &vulkanInstance, uint32_t &currentBufferIdx);
 void submitComputeBuffer(VkQueue queue, VkCommandBuffer computeCommandBuffer,
                          VkSemaphore graphicsSemaphore,
                          VkSemaphore computeSemaphore);
--- source/Submit.cpp
+++ source/Submit.cpp
@@ -1,21 +1,22 @@
-#include <VulkanTools/Submit.hpp>
 #include <VulkanTools/Initializers.hpp>
+#include <VulkanTools/Submit.hpp>
 namespace VkVP {
-void submitBuffers(VulkanInstance &vulkanInstance, uint32_t &currentBufferIdx) {
-  VK_CHECK_RESULT(vulkanInstance.swapChain.acquireNextImage(
-      vulkanInstance.semaphores.presentComplete, &currentBufferIdx));
-  vulkanInstance.submitInfo.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
-  vulkanInstance.submitInfo.commandBufferCount = 1;
-  vulkanInstance.submitInfo.pCommandBuffers =
-      &vulkanInstance.drawCmdBuffers[currentBufferIdx];
-  vulkanInstance.submitInfo.pNext = nullptr;
-  VK_CHECK_RESULT(vkQueueSubmit(vulkanInstance.queue, 1,
-                                &vulkanInstance.submitInfo, VK_NULL_HANDLE));
-  VK_CHECK_RESULT(vulkanInstance.swapChain.queuePresent(
-      vulkanInstance.queue, currentBufferIdx,
-      vulkanInstance.semaphores.renderComplete));
-  VK_CHECK_RESULT(vkQueueWaitIdle(vulkanInstance.queue));
-}
+// void submitBuffers(VulkanInstance &vulkanInstance, uint32_t
+// &currentBufferIdx) {
+//   VK_CHECK_RESULT(vulkanInstance.swapChain.acquireNextImage(
+//       vulkanInstance.semaphores.presentComplete, &currentBufferIdx));
+//   vulkanInstance.submitInfo.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
+//   vulkanInstance.submitInfo.commandBufferCount = 1;
+//   vulkanInstance.submitInfo.pCommandBuffers =
+//       &vulkanInstance.drawCmdBuffers[currentBufferIdx];
+//   vulkanInstance.submitInfo.pNext = nullptr;
+//   VK_CHECK_RESULT(vkQueueSubmit(vulkanInstance.queue, 1,
+//                                 &vulkanInstance.submitInfo, VK_NULL_HANDLE));
+//   VK_CHECK_RESULT(vulkanInstance.swapChain.queuePresent(
+//       vulkanInstance.queue, currentBufferIdx,
+//       vulkanInstance.semaphores.renderComplete));
+//   VK_CHECK_RESULT(vkQueueWaitIdle(vulkanInstance.queue));
+// }
 
 void submitComputeBuffer(VkQueue queue, VkCommandBuffer computeCommandBuffer,
                          VkSemaphore graphicsSemaphore,
@@ -39,8 +40,8 @@ void submitGraphicsBuffer(VulkanInstance &vulkanInstance,
                           VkSemaphore graphicsSemaphore,
                           VkSemaphore computeSemaphore) {
 
-  VkResult result = vulkanInstance.swapChain.acquireNextImage(
-      vulkanInstance.semaphores.presentComplete, &currentBuffer);
+  VK_CHECK_RESULT(vulkanInstance.swapChain.acquireNextImage(
+      vulkanInstance.semaphores.presentComplete, &currentBuffer));
 
   VkPipelineStageFlags graphicsWaitStageMasks[] = {
       VK_PIPELINE_STAGE_VERTEX_INPUT_BIT,
@@ -60,7 +61,11 @@ void submitGraphicsBuffer(VulkanInstance &vulkanInstance,
   submitInfo.pSignalSemaphores = graphicsSignalSemaphores;
   VK_CHECK_RESULT(
       vkQueueSubmit(vulkanInstance.queue, 1, &submitInfo, VK_NULL_HANDLE));
-  submitBuffers(vulkanInstance, currentBuffer);
+
+  VK_CHECK_RESULT(vulkanInstance.swapChain.queuePresent(
+      vulkanInstance.queue, currentBuffer,
+      vulkanInstance.semaphores.renderComplete));
+  VK_CHECK_RESULT(vkQueueWaitIdle(vulkanInstance.queue));
 }
 
 // submits compute-graphics semaphore synchronized command buffers
@@ -68,7 +73,7 @@ void submitBuffers(VulkanInstance &vulkanInstance,
                    VkCommandBuffer computeCommandBuffer,
                    VkSemaphore graphicsSemaphore,
                    VkSemaphore computeSemaphore) {
-  submitComputeBuffer(vulkanInstance.queue, computeCommandBuffer,
+  submitComputeBuffer(vulkanInstance.computeQueue, computeCommandBuffer,
                       graphicsSemaphore, computeSemaphore);
   submitGraphicsBuffer(vulkanInstance, vulkanInstance.currentBuffer,
                        graphicsSemaphore, computeSemaphore);
