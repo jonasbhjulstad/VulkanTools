45c8238b8de4e2496421de8b343ce444e2191538
Jonas Hjulstad <jonasbhjulstad@gmail.com>
2025-07-03 06:09:13 +0200

    Camera::getTransform

--- include/VulkanTools/Camera.hpp
+++ include/VulkanTools/Camera.hpp
@@ -17,6 +17,8 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/quaternion.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/quaternion.hpp>
 namespace VKT {
 	
 	struct Camera
